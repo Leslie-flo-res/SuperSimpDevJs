5bd012abcb2526351002f11500e79c55a0b863a2
Leslie-flo-res <Leslieflores28@gmail.com>
2025-03-04 10:29:27 -0700

    exercise 11c to  11f

--- JsCourse/11-arraysAndLoops-module/halfpointExercisesScript.js
+++ JsCourse/11-arraysAndLoops-module/halfpointExercisesScript.js
@@ -5,8 +5,34 @@ console.log(nums);
 
 //11b
 function getLastValue(array) {
-  let lastIndex = array.length - 1;
+  let lastIndex = array[array.length - 1];
   console.log(lastIndex);
 }
 
 console.log(getLastValue([1, 20, 22, 24, 5]));
+console.log(getLastValue(["hi", "hello", "good"]));
+
+//11c
+function arraySwap(array) {
+  let backwardsArr = array.reverse();
+  console.log(backwardsArr);
+}
+console.log(arraySwap([1, 20, 22, 24, 5]));
+console.log(arraySwap(["hi", "hello", "good"]));
+
+//11d
+for (let i = 0; i <= 10; i += 2) {
+  console.log(i);
+}
+
+//11e
+for (let i = 5; i >= 0; i--) {
+  console.log(i);
+}
+
+//11f
+let i = 0;
+while (i < 10) {
+  i += 2;
+  console.log(i);
+}
